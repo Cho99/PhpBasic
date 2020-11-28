d9c4fe05b02fe72b426787f44c19ae6947e4eca8
Chanh_Ubuntu2020 <quangsoi99@gmail.com>
2020-11-28 09:18:05 +0700

    Update Validate editNew, addNew

--- mvc/views/pages/addNew.php
+++ mvc/views/pages/addNew.php
@@ -1,9 +1,15 @@
 <div class="container_addNew">
     <div class="addNew">
-        <h2 style="text-align:center">Them Bai Viet</h2>
+        <h2 style="text-align:center">Add New</h2>
         <form action="http://localhost/php/News/store" method="POST">
             <label for="title">Title</label>
             <input type="text" id="title" name="title" placeholder="Title..." required>
+            <?php
+                if(isset($_SESSION['error'])) {
+                    echo "<span style='color: red; text-align: center; display:block; margin-bottom: 5px; font-size: 15px;'>".$_SESSION['error']."</span>";
+                    unset($_SESSION['error']);
+                }
+            ?>
             <label for="content">Content</label>
             <textarea id="subject" name="content" placeholder="Write something.." style="height:200px" required></textarea>
 
--- mvc/views/pages/editNew.php
+++ mvc/views/pages/editNew.php
@@ -10,6 +10,12 @@
             <input type="hidden" name="id" value="<?= $id ?>" > 
             <label for="title">Title</label>
             <input value="<?= $title ?>" type="text" id="title" name="title" placeholder="Title..."  required>
+            <?php
+                if(isset($_SESSION['error'])) {
+                    echo "<span style='color: red; text-align: center; display:block; margin-bottom: 5px; font-size: 15px;'>".$_SESSION['error']."</span>";
+                    unset($_SESSION['error']);
+                }
+            ?>
             <label for="content">Content</label>
             <textarea id="subject" name="content" placeholder="Write something.." style="height:200px" required><?= $content  ?></textarea>
 
